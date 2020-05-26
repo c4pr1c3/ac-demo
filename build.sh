4c0ac3e11356e96f37a264fb5d896d6116485bd6
c4pr1c3 <huangwei.me@cuc.edu.cn>
2020-05-26 16:34:14 +0800

    使用 matrix 方式重写 Actions

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t c4pr1c3/ac-demo-db:1.0  -f docker/mariadb/Dockerfile .
-docker build -t c4pr1c3/ac-demo-php:1.0  -f docker/php-apache/Dockerfile .
+docker build -t c4pr1c3/ac-demo-db:1.0  -f docker/ac-demo-db/Dockerfile .
+docker build -t c4pr1c3/ac-demo-php:1.0  -f docker/ac-demo-php/Dockerfile .
 docker run --rm -it -v "${PWD}/src:/src" -w /src node:11-alpine npm install
 docker-compose -f docker/docker-compose.yml up -d
